0bf2e5df13d75d64c3da96f246436335ec5e9243
kh <kris.n.harrison@gmail.com>
2025-02-04 20:56:57 -0500

    singly linked list append

--- linked_list_singly.py
+++ linked_list_singly.py
@@ -9,6 +9,12 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    def print_list(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data)
+            current_node = current_node.next
+
     def append(self, data):
         new_node = Node(data)
 
@@ -18,11 +24,19 @@ class LinkedList:
         
         # Called last node because this will eventually be pointing to the last node
         last_node = self.head
-        # Next node is not node
+        # Next node is not None
         while last_node.next:
-            # Assigned the next node in every iteraion
+            # Assigned the next node in every iterating
             # End result is that the last node of the linked list is assigned
             last_node = last_node.next
 
-        #Head is not at the last node
+        #Head is now at the last node
         last_node.next = new_node
+
+
+
+
+llist = LinkedList()
+llist.append("A")
+llist.append("B")
+llist.print_list()
