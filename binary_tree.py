7fb6c63e24899f4ef9dc3beb5c3e7ac425ac3dbc
kh <kris.n.harrison@gmail.com>
2025-04-24 14:45:13 -0400

    level order traversing

--- binary_tree.py
+++ binary_tree.py
@@ -1,4 +1,30 @@
 #Binary Tree Implementation
+#Three methods of depth first search
+
+#Queue class for level-order traversal of a binary tree
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def enqueue(self,item):
+        self.items.insert(0,item)
+
+    def dequeue(self):
+        if not self.is_empty():
+            return self.items.pop()
+
+    def is_empty(self):
+        return len(self.items) == 0
+    
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1].value
+
+    def __len__(self):
+        return self.size()
+    
+    def size(self):
+        return len(self.items)
 
 #Nodes have three components: value, pointer to the left child, pointer to the right child
 class Node:
@@ -21,6 +47,8 @@ class BinaryTree:
             return self.inorder_print(tree.root,"")
         elif traversal_type == "postorder":
             return self.postorder_print(tree.root, "")
+        elif traversal_type == "levelorder":
+            return self.level_order_print(tree.root)
         else:
             print("Traversal type " + str(traversal_type) + " is not supported.")
             return False
@@ -54,24 +82,50 @@ class BinaryTree:
             traversal = self.postorder_print(start.right, traversal)
             traversal += (str(start.value) + "-")
         return traversal
+    
+    def level_order_print(self,start):
+        if start is None:
+            return
 
+        queue = Queue()
+        queue.enqueue(start)
 
+        traversal = ""
+        while len(queue) > 0:
+            traversal += (str(queue.peek()) + "-")
+            node = queue.dequeue()
+
+            if node.left:
+                queue.enqueue(node.left)
+            if node.right:
+                queue.enqueue(node.right)
+        
+        return traversal
+
+                
 #Root of the tree
+#tree = BinaryTree(1)
+##Left child of root
+#tree.root.left = Node(2)
+##Right child of root
+#tree.root.right = Node(3)
+##Left child of 2
+#tree.root.left.left = Node(4)
+##Right child of 2
+#tree.root.left.right = Node(5)
+##Right child of 3
+#tree.root.right.left = Node(6)
+##Left child of 3
+#tree.root.right.right = Node(7)
+
+#Level Order Traversal Example
 tree = BinaryTree(1)
-#Left child of root
 tree.root.left = Node(2)
-#Right child of root
 tree.root.right = Node(3)
-#Left child of 2
 tree.root.left.left = Node(4)
-#Right child of 2
 tree.root.left.right = Node(5)
-#Right child of 3
-tree.root.right.left = Node(6)
-#Left child of 3
-tree.root.right.right = Node(7)
 
-
-print(tree.print_tree("preorder"))
-print(tree.print_tree("inorder"))
-print(tree.print_tree("postorder"))
+#print(tree.print_tree("preorder"))
+#print(tree.print_tree("inorder"))
+#print(tree.print_tree("postorder"))
+print(tree.print_tree("levelorder"))
